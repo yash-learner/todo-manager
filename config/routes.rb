37e53eacb468ee0d52c2cfea3b66671343e13286
yash-learner <yash.topaz@gmail.com>
2021-06-08 17:52:18 +0530

    Added create route with post learnt about the difference between post and get  request made a post request with postman Learnt that we can not make a post from browsers url so we postman added skip_before_authenticity in todoController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "todos", to: "todos#index"
-  get "todos/create", to: "todos#create"
+  post "todos/create", to: "todos#create"
   get "todos/:id", to: "todos#show"
 end
