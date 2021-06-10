4ac587fae1db793db6feca54e90e657e7440a75c
yash-learner <yash.topaz@gmail.com>
2021-06-10 08:34:01 +0530

    Added create method in user_controller.rb for user creation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
-    render plain: User.all.to_a
+    render plain: User.all.map {|user| user.to_displayable_string}
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    response_text = "Hey, your new user is created with id #{new_user.id}"
+    render plain: response_text
   end
 end
