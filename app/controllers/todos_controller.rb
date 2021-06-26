79f0387f1df7ee40ca4a90435cc4353383e7da66
yash-learner <yash.topaz@gmail.com>
2021-06-26 17:07:48 +0530

    Added ability to delete a todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -33,4 +33,11 @@ class TodosController < ApplicationController
     # render plain: "Updated todo completed status to #{completed}"
     redirect_to todos_path
   end
+
+  def destroy
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.destroy
+    redirect_to todos_path
+  end
 end
