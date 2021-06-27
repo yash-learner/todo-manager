117e196d510a7464733e177f59c8b5c4e5a13327
yash-learner <yash.topaz@gmail.com>
2021-06-27 23:20:53 +0530

    Added verify authentication

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def index
     # render plain: Todo.order(:due_date).map { |todo| todo.to_pleasent__string }.join("\n\n")
