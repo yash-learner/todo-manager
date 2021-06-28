ea4d90a37e5d6b438bbfea6b5763a81f8c7ddb1d
yash-learner <yash.topaz@gmail.com>
2021-06-28 15:49:41 +0530

    created users table using db:migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_170645) do
+ActiveRecord::Schema.define(version: 2021_06_28_101432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,11 +24,10 @@ ActiveRecord::Schema.define(version: 2021_06_21_170645) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
     t.string "first_name"
     t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
