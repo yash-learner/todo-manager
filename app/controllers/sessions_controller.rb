6a232b88d0315281aebe66c93bb9ccafdf5d3668
yash-learner <yash.topaz@gmail.com>
2021-07-01 00:11:40 +0530

    Added Signing-in updating session and fencing of protected routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
   def new
   end
 
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      render plain: "Correct Password"
+      session[:current_user_id] = user.id
+      redirect_to "/"
     else
       render plain: "Incorrect Password"
     end
