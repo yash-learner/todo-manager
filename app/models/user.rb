3f317b49e9418a7c52b2cdf6691c2a041e78bee4
yash-learner <yash.topaz@gmail.com>
2021-07-02 00:48:36 +0530

    Fixed password length and character validation error

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   def password_requirements
     rules = {
-      "is too short (minimum is 8 characters)"      => /^[a-zA-Z0-9]{8,}$/,
+      "is too short (minimum is 8 characters)"      => /[a-zA-Z0-9]|[A-Za-z0-9]|[0-9a-zA-Z]{8,}$/,
       "must contain at least one lowercase letter"  => /[a-z]+/,
       "must contain at least one uppercase letter"  => /[A-Z]+/,
       "must contain at least one digit"             => /\d+/,
