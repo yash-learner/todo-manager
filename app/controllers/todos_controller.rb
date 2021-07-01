ef35d5ad66eb0fe41372889ad3d2221692f0ec8b
yash-learner <yash.topaz@gmail.com>
2021-07-01 16:35:32 +0530

    Make app secure by scoping all actions only to the logged in user

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,12 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.of_user(current_user)
     render "index"
   end
 
   def show
     id = params[:id]
-    todo = Todo.find(id)
+    todo = Todo.of_user(current_user).find(id)
     render plain: todo.to_pleasent__string
   end
 
@@ -16,6 +17,7 @@ class TodosController < ApplicationController
       todo_text: todo_text,
       due_date: due_date,
       completed: false,
+      user_id: current_user.id
     )
     redirect_to todos_path
   end
@@ -23,7 +25,7 @@ class TodosController < ApplicationController
   def update
     id = params[:id]
     completed = params[:completed]
-    todo = Todo.find(id)
+    todo = Todo.of_user(current_user).find(id)
     todo.completed = completed
     todo.save!
     redirect_to todos_path
@@ -31,7 +33,7 @@ class TodosController < ApplicationController
 
   def destroy
     id = params[:id]
-    todo = Todo.find(id)
+    todo = Todo.where(user_id: current_user.id).find(id)
     todo.destroy
     redirect_to todos_path
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,13 @@ class UsersController < ApplicationController
 
 
   def create
-    User.create!(
+    user = User.create!(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password]
     )
+    session[:current_user_id] = user.id
     redirect_to  "/"
   end
 end
