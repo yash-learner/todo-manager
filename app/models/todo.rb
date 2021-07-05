44a3c595b905320526f49bb9862db6e5c07041ab
yash-learner <yash.topaz@gmail.com>
2021-07-05 19:07:09 +0530

    Corrected the method name in todo.rb

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -5,7 +5,7 @@ class Todo < ActiveRecord::Base
 
   belongs_to :user
 
-  def to_pleasent__string
+  def to_pleasent_string
     is_completed = completed ? "[x]" : "[ ]"
     "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
   end
